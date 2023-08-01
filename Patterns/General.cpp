7de4d300da1d2d8534266980524e9d497c2684aa
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-08-01 23:18:37 +0530

    Create General.cpp

---
+++ Patterns/General.cpp
@@ -0,0 +1,5 @@
+1. Sometimes the solution might look like it will give TLE for the given code, but the constraints are such that we dont need to run the code
+    for ultimate value of the constraints.
+
+
+   
