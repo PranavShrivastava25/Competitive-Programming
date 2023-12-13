12bb9274d2480174662586cab5bfbd2f6f7ae812
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-13 18:16:29 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,7 @@
+/*
+When we can do something with lets say a value 'k', and also with values larger than k,
+but not with values smaller than k, or vice versa
+If we want to find 'k'-- in such cases we can use BS.
+
+Eg- https://codeforces.com/contest/1907/problem/D
+*/
