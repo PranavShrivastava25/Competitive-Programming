7f005264429fde6f433efc3bf02d37f6d152cb5a
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-16 00:59:12 +0530

    Create GCDlcm.cpp

---
+++ GCDlcm.cpp
@@ -0,0 +1,15 @@
+/*
+Two numbers p and q.
+Their prime factorisation is represented as : 
+p= a1^b1 * a2^b3 * .....
+q= a1^b1 * a4^b4 * .....
+
+Then gcd = a1^min(b1,b2) * a2^min(b3,b4).....
+And lcm =  a1^max(b1,b2) * a2^max(b3,b4).....
+
+Note that b1,b2 ... can be >=0
+  
+*/
+Problem on this -> https://codeforces.com/problemset/problem/1866/B
+
+  
