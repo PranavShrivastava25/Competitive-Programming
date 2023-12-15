e114ac9fc30c10500832d143e7786554c7d48c24
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-15 21:24:15 +0530

    Update BitwiseOperations.cpp

--- BitwiseOperations.cpp
+++ BitwiseOperations.cpp
@@ -7,4 +7,5 @@
 If we add some fractions which are denoted as a expression = ( 1/2^k + 1/2^l +1/2^m + ..... ) , and this sums up to n/m, and none of l,m,n, are equa
 Then number of separate terms in the expresssion is equal to number of set bits in 'n'. 
 Because n will be a sum of unique powers of 2. , and these number of these unique powers is the number of set bits in binary exp of 'n'.
+Use __builtin_popcount(n)  for this
 */
