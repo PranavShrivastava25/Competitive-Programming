11d8a63843cea61934cab5ad0816a724610625af
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-15 16:01:32 +0530

    Create BitwiseOperations.cpp

---
+++ BitwiseOperations.cpp
@@ -0,0 +1,3 @@
+===== AND ====
+-> bitwise and of two numbers is always less than or equal to minimum of the two numbers.
+  Because one a bit becomes 0 in a number, it will always be 0, irrespective of whether the other number has 0 or 1 at that place.
