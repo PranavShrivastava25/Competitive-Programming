c3309c12f22b516cf1f22146fd9e5c9af0af9d5b
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-20 19:18:13 +0530

    Update BitwiseOperations.cpp

--- BitwiseOperations.cpp
+++ BitwiseOperations.cpp
@@ -1,5 +1,5 @@
 ===== AND ====
--> bitwise and of two numbers is always less than or equal to minimum of the two numbers.
+-> bitwise and of two numbers is always less than or equal to minimum of the two numbers, so and of n numbers <= minimmum number among them
   Because one a bit becomes 0 in a number, it will always be 0, irrespective of whether the other number has 0 or 1 at that place.
 
 
@@ -9,3 +9,11 @@ Then number of separate terms in the expresssion is equal to number of set bits 
 Because n will be a sum of unique powers of 2. , and these number of these unique powers is the number of set bits in binary exp of 'n'.
 Use __builtin_popcount(n)  for this
 */
+
+============XOR ====================
+-> if we have an expression  a^b^c^d and we want to undo xor of c from it  ,then we just need to again xor it with c.
+  --XOR IS THE UNDO OPERATION OF ITSELF 
+  
+-> Following this logic if we want to easily find the xor of a range of numbers in an array ;
+ we can use prefixxor array .
+   Then xor[a...b]= prefixxor[b]^prefixxor[a-1]
