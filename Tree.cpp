b2c070f9acab4190b539bf77bc4af9c534f00e04
Pranav Shrivastava <94039049+PranavShrivastava25@users.noreply.github.com>
2023-12-28 18:25:43 +0530

    Create Tree.cpp

---
+++ Tree.cpp
@@ -0,0 +1,8 @@
+// Finding diameter of a non negative weighted / unweighted tree
+--> Pick a node a , run a dfs and find the node at the farthest distance from a. Lets say node 'b' is at farthest distance from a.
+  Now run a dfs from 'b' and find the node at farthest distance from 'b' , lets say the node is c. 
+  Then  b--->c is the diameter of the tree
+  Proof :
+  Visulaise as  c-d--a->x-->y-->z--->p-->q-->b       ---> When we run dfs from a --> b will be farthest , 
+                       / \      \       /\                when we run dfs from b , c will be farthest 
+                      r   s     t      u  v
